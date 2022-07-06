3b7a96a995586391ee3e65ee874a5ad76c78c5b5
jacoposergio <djago32@gmail.com>
2022-07-06 18:16:06 +0200

    done

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -5,12 +5,12 @@
     </div>
     <div class="site-menu">
         <ul>
-            <li>
+            <li v-for="link, index in menuLinks" :key="index">
                 <div class="menu-img">
-                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <img :src= "require(`../assets/img/${link.img}`)" :alt="`pic di ${link.text}`">
                 </div>
                 <div class="menu-text">
-                    <h3>DIGITAL COMICS</h3>
+                    <p>{{link.text}}</p>
                 </div>
             </li>
         </ul>
@@ -20,7 +20,38 @@
 
 <script>
 export default {
-    name: 'PageMain'
+    name: 'PageMain',
+         data() {
+        return {
+            menuLinks: [
+                {
+                    text: 'DIGITAL COMICS',
+                    img: 'buy-comics-digital-comics.png',
+                    current: false
+                },
+                {
+                    text: 'DC MERCHANDISE',
+                    img: 'buy-comics-merchandise.png',
+                    current: true
+                },
+                {
+                    text: 'COMIC SHOP LOCATOR',
+                    img: 'buy-comics-shop-locator.png',
+                    current: false
+                },
+                {
+                    text: 'SUBSCRIPTIONS',
+                    img: 'buy-comics-subscriptions.png',
+                    current: false
+                },
+                {
+                    text: 'DC POWER VISA',
+                    img: 'buy-dc-power-visa.svg',
+                    current: false
+                }
+            ],
+        };
+     }
 }
 </script>
 
@@ -36,11 +67,31 @@ export default {
         }
         
         ul{
-            background-color: $brand_main_color;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                background-color: $brand_main_color;
+                padding: 1rem;
+                height: 200px;
+                padding: 0 3rem;
             li{
                 display: flex;
                 align-items: center;
-            }
+                color: white;
+                width: 150px;
+                margin-right: 3rem;
+
+                    .menu-img{
+                        max-width: calc((100%/5)*2);
+                        max-width: 40px;
+                    }
+
+                    .menu-text{
+                        width: calc((100%/5)*3);
+                        font-size: 0.7rem;
+                        margin-left: 1rem;
+                    }
+              }
         }
-  }
+    }
 </style>
